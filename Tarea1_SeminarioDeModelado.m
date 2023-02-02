e272ade7dab028584b60efee553a38b116ea6b6b
JulioCervantesGaray <88397317+JulioCervantesGaray@users.noreply.github.com>
2023-02-01 21:00:27 -0600

    Add files via upload

---
+++ Tarea1_SeminarioDeModelado.m
@@ -0,0 +1,14 @@
+%%-------Representación a bloques en Simulink de ecuaciones------------%%
+% ALBERTO JULIO CERVANTES GARAY %
+
+%% PARAMETROS %%
+
+m1 = 0.2;
+L1 = 0.3;
+m2 = 0.1;
+L2 = 0.25;
+g = 9.81;
+
+%% SIMULINK %%
+
+open('smlkT1.slx')
